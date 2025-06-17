cad574062e43160703d6e1f95972b9a29b8be1ae
Maria Achirei <maria.achirei@gmail.com>
2025-06-17 20:26:30 +0300

    rmv

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div id="survey-management">
+    <h1>School List</h1>
+    <button @click="fetchSchools">Get Schools</button>
+    <div v-if="error">{{ error }}</div>
+    <ul v-else>
+      <li v-for="school in schools" :key="school.Code">
+        <strong>{{ school.Name }}</strong> - {{ school.Teacher }} teaches {{ school.Subject }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      schools: [],
+      error: null
+    };
+  },
+  methods: {
+    fetchSchools() {
+      axios.get('http://localhost:3000/schools')
+        .then(response => {
+          this.schools = response.data.Data;
+        })
+        .catch(error => {
+          this.error = 'Failed to load schools: ' + error.message;
+        });
+    }
   }
 }
 </script>
 
 <style>
-#app {
+
+#survey-management {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- survey-management/README.md
+++ survey-management/README.md
@@ -1,4 +1,4 @@
-# frontend
+# survey-management
 
 ## Project setup
 ```
